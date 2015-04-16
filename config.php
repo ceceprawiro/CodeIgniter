4501e0b9408641934e7ba203ea784d81626dcd5a
Cecep Prawiro <cecep.prawiro@gmail.com>
2015-04-16 12:08:43 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DEBUG', true);
+
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'test');
+define('DB_DRIVER', 'mysql');
+
+define('KEY', 'C0d3igNi+Er3.0.o-st4B1E');
